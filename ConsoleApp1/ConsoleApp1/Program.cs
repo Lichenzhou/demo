2237388849b8bb739c3801733a69b63be0a85db0
浩林 梁 <695358154@qq.com>
2019-09-19 13:02:07 +0800

    1.0

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("PI is " + MathText.GetPi());
+            int x = MathText.GetSquareOf(5);
+            Console.WriteLine("square of  5 is " + x);
+
+            MathText math = new MathText();
+            math.value = 30;
+            Console.WriteLine("value field of math varlable contains " + math.value);
+            Console.WriteLine("square of 30 is " + math.GetSquare());
+        }
+
+        public class MathText
+        {
+            public int value;
+
+            public int GetSquare()
+            {
+
+
+                return value * value;
+            }
+            public static int GetSquareOf(int x)
+            {
+                return x * x;
+            }
+            public static double GetPi()
+            {
+                return 3.1415926;
+            }
+
+        }
+    }
+}
+
