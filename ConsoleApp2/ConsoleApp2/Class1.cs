050f23cfb0e0f374265da17100c2166f03de8dd2
浩林 梁 <695358154@qq.com>
2019-09-25 19:25:21 +0800

    匿名委托

--- ConsoleApp2/ConsoleApp2/Class1.cs
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -1,10 +1,61 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Bibliography;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ConsoleApp2
 {
-    class Class1
+    public class LinkedListNode<T>
     {
+        public LinkedListNode(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; private set; }
+        public LinkedListNode<T> Next { get; internal set; }
+        public LinkedListNode<T> Prev { get; internal set; }
     }
+    public class LinkedList<T>:IEnumerable<T>
+    {
+        public LinkedListNode<T> First { get; private set; }
+        public LinkedListNode<T> Last { get; private set; }
+        public LinkedListNode<T> AddLast(T node)
+        {
+            var newNode = new LinkedListNode<T>(node);
+            if(First==null)
+            {
+                First = newNode;
+                Last = First;
+            }
+            else
+            {
+                LinkedListNode<T> previous = Last;
+                Last.Next = newNode;
+                Last = newNode;
+                Last.Prev = previous;
+            }
+            return newNode;
+        }
+    }
+    public IEnumerable<T> GetEnumerator()
+    {
+        LinkedListNode<T> current = First;
+        while(current!=null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+        var list2 = new LinkedListNode<int>();
+        list2.AddLast(1);
+        list2.AddLast(3);
+        list2.AddLast(5);
+
+
+
 }
